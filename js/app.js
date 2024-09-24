f85de58ca7644166e8eb30e481011c43bedc01c7
ktr.krose <ktr.krose@gmail.com>
2024-09-24 12:38:12 -0400

    began init function

--- js/app.js
+++ js/app.js
@@ -2,10 +2,31 @@
 
 /*------Variables------*/
 let cards = [];
+console.log(cards)
+
 let matchingCards = 0;
+console.log(matchingCards)
+
 let playerPicks = [];
+console.log(playerPicks)
+
 let winner;
+console.log(winner)
 /*------Functions------*/
+// init()
+
+const init = () => {
+    winner = false;
+    matchingCards = 0;
+    playerPicks = [];
+}
+console.log(init)
+
+const render = () => {}
+
+const compareCards = () => {}
+
+const shuffleCards = () => {}
 
 /*------Event Listeners------*/
 
