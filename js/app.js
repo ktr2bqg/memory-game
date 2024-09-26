acd9322f5a815307b5b3a3df41aa98176d5a15e5
ktr.krose <ktr.krose@gmail.com>
2024-09-25 21:48:33 -0400

    update flip card function

--- js/app.js
+++ js/app.js
@@ -1,44 +1,52 @@
 /*------Constants------*/
 const cardEls = document.querySelectorAll(".cards");
-console.log(cardEls)
-
-const timerEl = document.querySelector("h2")
-console.log(timerEl)
-
+console.log(cardEls);
+const timerEl = document.querySelector("h2");
 const messageEl = document.querySelector("h3");
-console.log(messageEl)
-
 const playBtn = document.getElementById("play-again");
-console.log(playBtn)
+
 /*------Variables------*/
 let cards = [];
-console.log(cards)
-
 let matchingCards = 0;
-console.log(matchingCards)
-
 let playerPicks = [];
-console.log(playerPicks)
-
 let winner;
-console.log(winner)
+
 /*------Functions------*/
-// init()
+// State of the cards
+init()
 
-const init = () => {
-    winner = false;
-    matchingCards = 0;
-    playerPicks = [];
+// INITLIZE FUNCTION
+function init () {
+    cardEls.forEach((card) => {
+    card.isFacedown = true; 
+    })
 }
-console.log(init)
 
-const render = () => {}
+// FUNCTION 2
+const flipCard = (card) => {
+  if (card.isFacedown) {
+    card.isFacedown = false; // change to face-up
+    card.style.backgroundColor = "red"; // change background color
+    console.log("Card flipped to face up");
+  } else {
+    console.log("Card is already face up");
+  }
+};
 
-const pairingColors = () => {}
+// FUNCTION 3
 
-const compareCards = () => {}
-
-const shuffleCards = () => {}
+const itsAMatch = () => {
+    const [card1, card2] = playerPicks;
+}
 
 /*------Event Listeners------*/
+// Play button event listener
+playBtn.addEventListener("click", function () {
+  console.log("Button was clicked");
+});
 
+cardEls.forEach((card) => {
+  card.addEventListener("click", function () {
+    flipCard(card);
+  });
+});
