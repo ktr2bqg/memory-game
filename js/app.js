71ddbb0829a694cc4174e22d3d6400c240c2099c
ktr.krose <ktr.krose@gmail.com>
2024-09-28 23:43:08 -0400

    hues of blues complete

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ function init() {
   const shuffledColors = shuffleColors(makePairs(colors));
 
   cardEls.forEach((card, idx) => {
-    card.isFacedown = true; // flip card up
+    card.isFacedown = true;
     card.style.backgroundColor = "";
     card.color = shuffledColors[idx];
     card.classList.remove("flipped");
